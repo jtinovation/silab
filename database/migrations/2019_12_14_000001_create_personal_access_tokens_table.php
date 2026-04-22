d3485c6170c3429d06fc1714633ffe1aefac41b8
DhaniPutraa <dhani101004@gmail.com>
2026-04-22 10:34:08 +0700

    Fix: Finalize core system migrations sync (PassResets, FailedJobs, Tokens)

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -12,17 +12,19 @@ class CreatePersonalAccessTokensTable extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('personal_access_tokens', function (Blueprint $table) {
+        $table->id();
+        $table->string('tokenable_type', 191);
+        $table->unsignedBigInteger('tokenable_id');
+        $table->index(['tokenable_type', 'tokenable_id']); 
+        $table->string('name', 191); 
+        $table->string('token', 64)->unique();
+        $table->text('abilities')->nullable();
+        $table->timestamp('last_used_at')->nullable();
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
